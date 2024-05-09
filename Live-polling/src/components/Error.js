54430753f679a413df4244a3f9037bce046e637d
Mohammed Qubaisuddin <79993269+md-qubais@users.noreply.github.com>
2024-05-08 21:02:02 -0500

    Add files via upload

---
+++ Live-polling/src/components/Error.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Error() {
+	return (
+		<>
+			<Header />
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<h1>Error:404!! Page not found</h1>
+			</div>
+			<Footer />
+		</>
+	);
+}
---
+++ Live-polling/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<div className="footer">
+			<p className="footer-text">
+				Built with ❤ by{" "}
+				<a
+					className="profile"
+					href="https://github.com/Md-Abdul-Hameed"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Hameed
+				</a>
+			</p>
+		</div>
+	);
+}
